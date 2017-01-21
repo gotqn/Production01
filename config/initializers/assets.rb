2e985aafdc7b178045dbc18e8b93a36cea508cd6
gotqn <george_27@abv.bg>
2017-01-21 09:34:35 +0200

    capistrano 4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,6 @@ Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
 
 # Fonts
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
+# all files
+Rails.application.config.assets.precompile += ['*.js', '*.css', '**/*.js', '**/*.css']
