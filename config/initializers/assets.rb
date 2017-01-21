e2246239b97c2e8eb4638002a918d7ac181cf3ee
gotqn <george_27@abv.bg>
2017-01-21 09:24:53 +0200

    capistrano 4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Images
+Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
+
+# Fonts
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
